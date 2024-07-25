e0c1acd8427e1a2d3b7e03e8f684a7ea1c306eb0
Haonan <guanhn1214@gmail.com>
2024-07-25 00:10:50 -0400

    Added Caching for CartService and RestaurantService, added Unit test CartServiceTests

--- src/main/java/com/laioffer/onlineorder/service/CartService.java
+++ src/main/java/com/laioffer/onlineorder/service/CartService.java
@@ -8,6 +8,8 @@ import com.laioffer.onlineorder.model.OrderItemDto;
 import com.laioffer.onlineorder.repository.CartRepository;
 import com.laioffer.onlineorder.repository.MenuItemRepository;
 import com.laioffer.onlineorder.repository.OrderItemRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,6 +32,7 @@ public class CartService {
         this.orderItemRepository = orderItemRepository;
     }
 
+    @CacheEvict(cacheNames = "cart", key = "#customerId")
     @Transactional
     public void addMenuItemToCart(long customerId, long menuItemId) {
         CartEntity cart = cartRepository.getByCustomerId(customerId);
@@ -51,6 +54,7 @@ public class CartService {
         cartRepository.updateTotalPrice(cart.id(), cart.totalPrice() + menuItem.price());
     }
 
+    @Cacheable(cacheNames = "cart", key = "#customerId")
     public CartDto getCart(Long customerId) {
         CartEntity cart = cartRepository.getByCustomerId(customerId);
         List<OrderItemEntity> orderItems = orderItemRepository.getAllByCartId(cart.id());
@@ -58,6 +62,7 @@ public class CartService {
         return new CartDto(cart, orderItemDtos);
     }
 
+    @CacheEvict(cacheNames = "cart", key = "#customerId")
     @Transactional
     public void clearCart(Long customerId) {
         CartEntity cartEntity = cartRepository.getByCustomerId(customerId);
--- src/main/java/com/laioffer/onlineorder/service/RestaurantService.java
+++ src/main/java/com/laioffer/onlineorder/service/RestaurantService.java
@@ -6,6 +6,7 @@ import com.laioffer.onlineorder.model.MenuItemDto;
 import com.laioffer.onlineorder.model.RestaurantDto;
 import com.laioffer.onlineorder.repository.MenuItemRepository;
 import com.laioffer.onlineorder.repository.RestaurantRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -26,6 +27,7 @@ public class RestaurantService {
         this.menuItemRepository = menuItemRepository;
     }
 
+    @Cacheable("restaurants")
     public List<RestaurantDto> getRestaurants() {
         List<RestaurantEntity> restaurantEntities = restaurantRepository.findAll();
         List<MenuItemEntity> menuItemEntities = menuItemRepository.findAll();
